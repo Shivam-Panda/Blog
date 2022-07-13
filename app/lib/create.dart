b1e32f2071b5bbea6d9c71fbaf941c7430cec000
Shivam Panda <panda.shivam81@bloomfield.org>
2022-07-13 17:35:31 -0400

    Create Page Done, Project is Done

--- app/lib/create.dart
+++ app/lib/create.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CreatePage extends StatefulWidget {
   @override
@@ -6,20 +9,58 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
-  int _counter = 0;
+  String post_author = '', post_body = '', post_title = '';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Widget s = Container();
+
+  void makePost() {
+    DateTime _now = DateTime.now();
+    String timestamp =
+        '${_now.hour}:${_now.minute}:${_now.second}.${_now.millisecond}';
+
+    dynamic s = http.post(
+        'https://blog-flask-api-python.herokuapp.com/makePost',
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': 'PostmanRuntime/7.29.0'
+        },
+        body: json.encode({
+          "author": this.post_author,
+          "body": this.post_body,
+          "time": timestamp,
+          "title": this.post_title
+        }));
+    s.then((v) => {
+          setState(() {
+            this.s = Text('Submitted, move to the Posts page');
+          })
+        });
   }
 
   @override
   Widget build(BuildContext c) {
-    return Row(
+    return Column(
       children: [
-        Text('$_counter'),
-        TextButton(onPressed: _incrementCounter, child: Text("Click Me"))
+        // Text('$_counter'),
+        // TextButton(onPressed: _incrementCounter, child: Text("Click Me"))
+        TextField(
+          decoration: InputDecoration(labelText: 'Author'),
+          onSubmitted: (String s) {
+            post_author = s;
+          },
+        ),
+        TextField(
+            decoration: InputDecoration(labelText: 'Body'),
+            onSubmitted: (String value) {
+              post_body = value;
+            }),
+        TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            onSubmitted: (String value) {
+              post_title = value;
+            }),
+        TextButton(onPressed: makePost, child: Text('Submit')),
+        this.s
       ],
     );
   }
